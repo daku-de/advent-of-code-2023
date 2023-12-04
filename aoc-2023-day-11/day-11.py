efcfa517e468973ea16abf380e098d7a1c51febb
Daniel Kurtz <daniel.kurtz@fau.de>
2023-12-04 22:31:57 +0900

    Updated skeleton

---
+++ aoc-2023-day-11/day-11.py
@@ -0,0 +1,37 @@
+"""
+Advent of Code 2023 - Day 11
+
+Start: 11. Dec 2023 - 
+Finish: 11. Dec 2023 - 
+Notes:
+    
+"""
+
+
+def read_data():
+    with open("data", "r") as file:
+        for line in file:
+            pass
+
+
+def part1():
+    print("⋆⁺₊❅⋆ ⁺₊❆⋆⋆⁺₊❅⋆ ⁺₊❆⋆")
+    print("Solution Part 1")
+
+
+def part2():
+    print("⋆⁺₊❅⋆ ⁺₊❆⋆⋆⁺₊❅⋆ ⁺₊❆⋆")
+    print("Solution Part 2")
+
+
+def main():
+    print("⋆⁺₊❅⋆₊☃️ 🎄 ☃️₊❅⁺₊❆⋆")
+    print("Solution for Day 11")
+    part1()
+    part2()
+    print("⋆⁺₊❅⋆ ⁺₊❆⋆⋆⁺₊❅⋆ ⁺₊❆⋆")
+
+
+if __name__ == "__main__":
+    main()
+
